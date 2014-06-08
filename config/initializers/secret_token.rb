87e992befcb4a6bfcfa59feee4d5cb839a420173
Ryoto Maeda <ruder.com@gmail.com>
2014-06-09 06:53:47 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Coratta::Application.config.secret_key_base = '0d84ea434a67c6883c560f96fabc322e754a96ab70fe3f095f766ac85aece2961fb915307f3a3663f24fbe930cdd91b96d3dd85a1acf142e3ac4eeaeea3fcfb6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Coratta::Application.config.session_store :cookie_store, key: '_coratta_session'
